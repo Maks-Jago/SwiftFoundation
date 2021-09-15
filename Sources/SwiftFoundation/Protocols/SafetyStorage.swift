971078ec5e6bc0c7326fef94fe7e5872cf84df85
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2021-09-15 12:54:24 +0300

    update SafetyValue

--- Sources/SwiftFoundation/Protocols/SafetyStorage.swift
+++ Sources/SwiftFoundation/Protocols/SafetyStorage.swift
@@ -17,19 +17,21 @@ public protocol SafetyStorage {
 }
 
 @propertyWrapper
-public final class SafetyValue<T: Codable> {
+public struct SafetyValue<T: Codable> {
     public var defaultValue: T
     public var key: String
-    public var storage: SafetyStorage
-    
+    @EquatableNoop public var storage: SafetyStorage
+
     public init(key: String, defaultValue: T, storage: SafetyStorage) {
         self.key = key
         self.defaultValue = defaultValue
         self.storage = storage
     }
-    
+
     public var wrappedValue: T {
         get { (try? storage.load(key: key)) ?? defaultValue }
         set { try? storage.save(newValue, key: key) }
     }
 }
+
+extension SafetyValue: Equatable where T: Equatable {}
