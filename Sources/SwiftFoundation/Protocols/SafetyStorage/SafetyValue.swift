fc104fe6cd0796873c6e921b0a2c570b15a6659c
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2021-09-27 19:48:20 +0300

    SafetyValue custom equatable

--- Sources/SwiftFoundation/Protocols/SafetyStorage/SafetyValue.swift
+++ Sources/SwiftFoundation/Protocols/SafetyStorage/SafetyValue.swift
@@ -12,17 +12,29 @@ public struct SafetyValue<T: Codable> {
     public var defaultValue: T
     public var key: String
     @EquatableNoop public var storage: SafetyStorage
+    private var inMemoryValue: T
 
     public init(key: String, defaultValue: T, storage: SafetyStorage) {
         self.key = key
         self.defaultValue = defaultValue
         self.storage = storage
+        self.inMemoryValue = defaultValue
+        self.inMemoryValue = self.wrappedValue
     }
 
     public var wrappedValue: T {
         get { (try? storage.load(key: key)) ?? defaultValue }
-        set { try? storage.save(newValue, key: key) }
+        set {
+            inMemoryValue = newValue
+            try? storage.save(newValue, key: key)
+        }
     }
 }
 
-extension SafetyValue: Equatable where T: Equatable {}
+extension SafetyValue: Equatable where T: Equatable {
+    public static func == (lhs: SafetyValue<T>, rhs: SafetyValue<T>) -> Bool {
+        lhs.key == rhs.key &&
+        lhs.inMemoryValue == rhs.inMemoryValue &&
+        lhs.defaultValue == rhs.defaultValue
+    }
+}
