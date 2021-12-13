2ac521e8a589c625af121444fc1703948b13e4a3
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2021-12-13 18:28:23 +0200

    remove SafetyValue

--- Sources/SwiftFoundation/Protocols/SafetyStorage/StorableValue.swift
+++ Sources/SwiftFoundation/Protocols/SafetyStorage/StorableValue.swift
@@ -36,36 +36,3 @@ public struct StorableValue<T: Codable> {
 }
 
 extension StorableValue: Equatable where T: Equatable {}
-
-@available(*, deprecated, message: "Use `StorableValue` instead of SafetyValue")
-@propertyWrapper
-public struct SafetyValue<T: Codable> {
-    public var defaultValue: T
-    public var key: String
-    @EquatableNoop public var storage: SafetyStorage
-    private var inMemoryValue: T
-
-    public init(key: String, defaultValue: T, storage: SafetyStorage) {
-        self.key = key
-        self.defaultValue = defaultValue
-        self.storage = storage
-        self.inMemoryValue = defaultValue
-        self.inMemoryValue = self.wrappedValue
-    }
-
-    public var wrappedValue: T {
-        get { (try? storage.load(key: key)) ?? defaultValue }
-        set {
-            inMemoryValue = newValue
-            try? storage.save(newValue, key: key)
-        }
-    }
-}
-
-extension SafetyValue: Equatable where T: Equatable {
-    public static func == (lhs: SafetyValue<T>, rhs: SafetyValue<T>) -> Bool {
-        lhs.key == rhs.key &&
-        lhs.inMemoryValue == rhs.inMemoryValue &&
-        lhs.defaultValue == rhs.defaultValue
-    }
-}
