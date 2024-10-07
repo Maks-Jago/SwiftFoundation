0aaa5cad80ec7201394a961d4f701a6b085ecbf1
Oksana Fedorchuk <oksanafed.dev@gmail.com>
2024-10-07 11:10:53 +0300

    Documentation location extensions

--- Sources/SwiftFoundation/CoreLocation/CNMutablePostalAddress.swift
+++ Sources/SwiftFoundation/CoreLocation/CNMutablePostalAddress.swift
@@ -1,16 +1,22 @@
+//===--- CNMutablePostalAddress.swift -------------------------------------===//
 //
-//  CNMutablePostalAddress.swift
-//  SwiftFoundation
+// This source file is part of the SwiftFoundation open source project
 //
-//  Created by Max Kuznetsov on 25.10.2020.
+// Copyright (c) 2024 You Are Launched
+// Licensed under Apache License v2.0
 //
+// See https://opensource.org/licenses/Apache-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 import CoreLocation
 import Contacts
 
 public extension CNMutablePostalAddress {
-    /// Constructs a `CNMutablePostalAddress` from a `CLPlacemark`
+    
+    /// Initializes a `CNMutablePostalAddress` from a `CLPlacemark`.
+    /// - Parameter placemark: A `CLPlacemark` object containing the postal address information.
     convenience init(placemark: CLPlacemark) {
         self.init()
         self.street = (placemark.subThoroughfare ?? "") + " " + (placemark.thoroughfare ?? "")
